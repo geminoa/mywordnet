1c57f74a4e4c698544611133c983de607302816e
yo <verdigrisbrain_en@yahoo.com>
2014-12-22 16:52:48 +0900

    Apart Structs from main lib mywn.rb to make it simple

---
+++ lib/mywn_structs.rb
@@ -0,0 +1,55 @@
+
+MyWord = Struct.new(
+  "MyWord",
+  :wordid,  # 単語ID
+  :lang,    # eng or jpn
+  :lemma,   # 見出し語
+  :pron,    # 発音
+  :pos,     # 品詞
+  :synsets  # 類義語
+)
+
+MySynset = Struct.ew(
+  "MySynset",
+  :pos,     # 品詞
+  :name,
+  :def,
+  :name_jp
+)
+
+Word = Struct.new(
+  "Word",
+  :wordid,
+  :lang,
+  :lemma,
+  :pron,    # 発音
+  :pos     # 品詞
+)
+
+Sense = Struct.new(
+  "Sense",
+  :synset,
+  :wordid,
+  :lang,
+  :rank,
+  :lexid,
+  :freq,
+  :src
+)
+
+Synset = Struct.new(
+  "Synset",
+  :synset,
+  :pos,     # 品詞
+  :name,
+  :src
+)
+
+SynLink = Struct.new(
+  "SynLink",
+  :synset1,
+  :synset2,
+  :link,
+  :src
+)
+
